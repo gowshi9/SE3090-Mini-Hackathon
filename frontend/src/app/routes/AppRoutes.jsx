c896f58b0b2e6ed6f8a92bbf842065751b955e6e
MathusanM <manokumarmathu@gmail.com>
2026-09-04 11:41:47 +0530

    Implement food discovery and details flow

--- frontend/src/app/routes/AppRoutes.jsx
+++ frontend/src/app/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import { FindFoodPage } from '../../features/find-food/pages/FindFoodPage';
+import { FoodDetailsPage } from '../../features/find-food/pages/FoodDetailsPage';
 import { PostFoodPage } from '../../features/post-food/pages/PostFoodPage';
 import { ReservationPage } from '../../features/reservation/pages/ReservationPage';
 import { DashboardPage } from '../../features/dashboard/pages/DashboardPage';
@@ -17,6 +18,7 @@ export function AppRoutes() {
       <Route path="/post-food" element={<PostFoodPage />} />
       <Route path="/reserve/:id" element={<ReservationPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/food/:id" element={<FoodDetailsPage />} />
     </Routes>
   );
 }
